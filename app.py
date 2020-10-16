17a6df17152247a8429a841987fd4e7f5567140c
Thillai S <thillair@yahoo.com>
2020-10-16 23:35:42 +0530

    modified div ht width

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
         st.image(svg)
         st.markdown(f' lines {st_num_lines}, color1 {st_col1}, color2 {st_col2}')
 	
-        html = f""" <div style="background-color: {get_color()}; height: 15px; width: 100px; "> 
+        html = f""" <div style="background-color: {get_color()}; height: 10px; width: 10px; "> 
 	            </div>
 		"""
         components.html(html, height=100, width=100, scrolling=False)
